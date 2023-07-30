8abcb16a2db6a755f25ebc584fbf49497cb7c84f
Lucas Vieira <lucas_99silva@hotmail.com>
2023-07-30 13:23:44 -0300

    ensure AddPlant calls AddPlantRepository with correct data

---
+++ src/data/usecases/add-plant.spec.ts
@@ -0,0 +1,39 @@
+import { type Plant } from '@/domain/entities'
+
+class AddPlant {
+  constructor(private readonly addPlantRepository: AddPlantRepository) {}
+
+  async perform(input: Plant): Promise<void> {
+    await this.addPlantRepository.load(input)
+  }
+}
+
+interface AddPlantRepository {
+  load: (input: Plant) => Promise<void>
+}
+
+class AddPlantRepositoryMock implements AddPlantRepository {
+  input?: Plant
+
+  async load(input: Plant): Promise<void> {
+    this.input = input
+  }
+}
+
+describe('AddPlant UseCase', () => {
+  it('should call AddPlantRepository with correct data', async () => {
+    const addPlantRepositoryMock = new AddPlantRepositoryMock()
+    const sut = new AddPlant(addPlantRepositoryMock)
+    const plant = {
+      id: 'any_id',
+      name: 'any_plant',
+      description: 'any_plant_description',
+      waterTips: 'any_water_tips',
+      photo: 'any_photo',
+    }
+
+    await sut.perform(plant)
+
+    expect(addPlantRepositoryMock.input).toEqual(plant)
+  })
+})
