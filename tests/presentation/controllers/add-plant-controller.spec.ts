c1ded2e6328a513212ca18fc7a193dc29b2585fc
Lucas Vieira <lucas_99silva@hotmail.com>
2023-09-05 14:57:11 -0300

    feat: ensure AddPlantController returns 500 if AddPlant throws

--- tests/presentation/controllers/add-plant-controller.spec.ts
+++ tests/presentation/controllers/add-plant-controller.spec.ts
@@ -5,15 +5,34 @@ interface HttpRequest {
   body?: any
 }
 
+interface HttpResponse {
+  statusCode: number
+  data?: any
+}
+
 class AddPlantController {
   constructor(private readonly addPlant: AddPlant) {}
 
-  async handle(httpRequest: HttpRequest): Promise<void> {
-    await this.addPlant.perform(httpRequest.body)
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      await this.addPlant.perform(httpRequest.body)
+      return null as any
+    } catch (error) {
+      return {
+        statusCode: 500,
+        data: {
+          error: {
+            name: 'InternalServerError',
+            message:
+              'An internal server error occurred while processing the request.',
+          },
+        },
+      }
+    }
   }
 }
 
-class AddPlantMock implements AddPlant {
+class AddPlantSpy implements AddPlant {
   input?: PlantParams
 
   async perform(input: PlantParams): Promise<boolean> {
@@ -24,8 +43,8 @@ class AddPlantMock implements AddPlant {
 
 describe('AddPlant Controller', () => {
   it('should call AddPlant with correct data', async () => {
-    const addPlantMock = new AddPlantMock()
-    const sut = new AddPlantController(addPlantMock)
+    const addPlantSpy = new AddPlantSpy()
+    const sut = new AddPlantController(addPlantSpy)
     const httpRequest = {
       body: {
         name: faker.lorem.words(),
@@ -38,6 +57,36 @@ describe('AddPlant Controller', () => {
 
     await sut.handle(httpRequest)
 
-    expect(addPlantMock.input).toEqual(httpRequest.body)
+    expect(addPlantSpy.input).toEqual(httpRequest.body)
+  })
+
+  it('should return 500 if AddPlant throws', async () => {
+    const addPlantSpy = new AddPlantSpy()
+    jest.spyOn(addPlantSpy, 'perform').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const sut = new AddPlantController(addPlantSpy)
+    const httpRequest = {
+      body: {
+        name: faker.lorem.words(),
+        description: faker.lorem.words(),
+        waterTips: faker.lorem.words(),
+        photo: faker.internet.avatar(),
+        plantWaterFrequencyId: faker.string.uuid(),
+      },
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 500,
+      data: {
+        error: {
+          name: 'InternalServerError',
+          message:
+            'An internal server error occurred while processing the request.',
+        },
+      },
+    })
   })
 })
