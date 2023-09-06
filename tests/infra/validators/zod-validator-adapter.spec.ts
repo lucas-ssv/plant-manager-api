d2af4e280e510d36e34b7e19aada58534474ffdb
Lucas Vieira <lucas_99silva@hotmail.com>
2023-09-06 12:04:28 -0300

    feat: ensure ZodValidatorAdapter returns false if field is not number

--- tests/infra/validators/zod-validator-adapter.spec.ts
+++ tests/infra/validators/zod-validator-adapter.spec.ts
@@ -5,22 +5,38 @@ class ZodValidatorAdapter {
   isString(fieldValue: any): boolean {
     return z.string().safeParse(fieldValue).success
   }
+
+  isNumber(fieldValue: any): boolean {
+    return false
+  }
 }
 
 describe('ZodValidator Adapter', () => {
-  it('should return false if field value is not string', () => {
-    const sut = new ZodValidatorAdapter()
+  describe('isString()', () => {
+    it('should return false if field value is not string', () => {
+      const sut = new ZodValidatorAdapter()
 
-    const isString = sut.isString(faker.number.int())
+      const isString = sut.isString(faker.number.int())
 
-    expect(isString).toBe(false)
+      expect(isString).toBe(false)
+    })
+
+    it('should return true if field value is string', () => {
+      const sut = new ZodValidatorAdapter()
+
+      const isString = sut.isString(faker.string.alpha())
+
+      expect(isString).toBe(true)
+    })
   })
 
-  it('should return true if field value is string', () => {
-    const sut = new ZodValidatorAdapter()
+  describe('isNumber()', () => {
+    it('should return false if field value is not number', () => {
+      const sut = new ZodValidatorAdapter()
 
-    const isString = sut.isString(faker.string.alpha())
+      const isNumber = sut.isNumber(faker.string.alpha())
 
-    expect(isString).toBe(true)
+      expect(isNumber).toBe(false)
+    })
   })
 })
