a7e47384946c84893ed1f2588e81fd36b4cff8e2
Lucas Vieira <lucas_99silva@hotmail.com>
2023-09-06 13:56:39 -0300

    feat: ensure AddPlantController calls Validation with correct data

--- src/presentation/controllers/add-plant-controller.ts
+++ src/presentation/controllers/add-plant-controller.ts
@@ -8,10 +8,14 @@ import {
 import { noContent, serverError } from '@/presentation/helpers'
 
 export class AddPlantController implements Controller {
-  constructor(private readonly addPlant: AddPlant) {}
+  constructor(
+    private readonly validation: any,
+    private readonly addPlant: AddPlant
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       await this.addPlant.perform(httpRequest.body)
       return noContent()
     } catch (error) {
--- tests/presentation/controllers/add-plant-controller.spec.ts
+++ tests/presentation/controllers/add-plant-controller.spec.ts
@@ -15,10 +15,30 @@ const mockRequest = (): HttpRequest => ({
   },
 })
 
+class ValidationMock {
+  input?: object
+
+  validate(input: object): void {
+    this.input = input
+  }
+}
+
 describe('AddPlant Controller', () => {
-  it('should call AddPlant with correct data', async () => {
+  it('should call Validation with correct data', async () => {
+    const validationMock = new ValidationMock()
     const addPlantSpy = new AddPlantSpy()
-    const sut = new AddPlantController(addPlantSpy)
+    const sut = new AddPlantController(validationMock, addPlantSpy)
+    const httpRequest = mockRequest()
+
+    await sut.handle(httpRequest)
+
+    expect(validationMock.input).toEqual(httpRequest.body)
+  })
+
+  it('should call AddPlant with correct data', async () => {
+    const validationMock = new ValidationMock()
+    const addPlantSpy = new AddPlantSpy()
+    const sut = new AddPlantController(validationMock, addPlantSpy)
     const httpRequest = mockRequest()
 
     await sut.handle(httpRequest)
@@ -27,11 +47,12 @@ describe('AddPlant Controller', () => {
   })
 
   it('should return 500 if AddPlant throws', async () => {
+    const validationMock = new ValidationMock()
     const addPlantSpy = new AddPlantSpy()
     jest.spyOn(addPlantSpy, 'perform').mockImplementationOnce(() => {
       throw new Error()
     })
-    const sut = new AddPlantController(addPlantSpy)
+    const sut = new AddPlantController(validationMock, addPlantSpy)
 
     const httpResponse = await sut.handle(mockRequest())
 
@@ -39,8 +60,9 @@ describe('AddPlant Controller', () => {
   })
 
   it('should return 204 if AddPlant succeeds', async () => {
+    const validationMock = new ValidationMock()
     const addPlantSpy = new AddPlantSpy()
-    const sut = new AddPlantController(addPlantSpy)
+    const sut = new AddPlantController(validationMock, addPlantSpy)
 
     const httpResponse = await sut.handle(mockRequest())
 
