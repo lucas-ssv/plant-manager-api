38a7fb8168d91741126b0cf503e1ffaaa5170a95
Lucas Vieira <lucas_99silva@hotmail.com>
2023-10-19 15:01:08 -0300

    feat: ensure LoadPlantsController returns a list of plants on success

--- tests/presentation/controllers/load-plants-controller.spec.ts
+++ tests/presentation/controllers/load-plants-controller.spec.ts
@@ -5,22 +5,51 @@ import {
   type HttpRequest,
   type HttpResponse,
 } from '@/presentation/contracts'
+import { faker } from '@faker-js/faker'
 
 class LoadPlantsController implements Controller {
   constructor(private readonly loadPlants: LoadPlants) {}
 
   async handle(HttpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadPlants.perform()
+    const plants = await this.loadPlants.perform()
+    return {
+      statusCode: 200,
+      body: plants,
+    }
   }
 }
 
 class DbLoadPlantsMock implements LoadPlants {
   callsCount = 0
+  output = mockPlants()
 
   async perform(): Promise<Plant[]> {
     this.callsCount++
+    return this.output
   }
 }
+
+const mockPlants = (): Plant[] => [
+  {
+    id: faker.string.uuid(),
+    name: faker.lorem.word(),
+    description: faker.lorem.words(),
+    waterTips: faker.lorem.words(),
+    photo: faker.internet.avatar(),
+    plantWaterFrequency: {
+      id: faker.string.uuid(),
+      title: faker.lorem.words(),
+      time: faker.string.numeric(),
+      gap: faker.number.int(),
+    },
+    environments: [
+      {
+        id: faker.string.uuid(),
+        title: faker.lorem.word(),
+      },
+    ],
+  },
+]
 
 describe('LoadPlantsController', () => {
   it('should call DbLoadPlants', async () => {
@@ -30,5 +59,19 @@ describe('LoadPlantsController', () => {
     await sut.handle({})
 
     expect(loadPlantsMock.callsCount).toBe(1)
+  })
+
+  it('should return a list of plants on success', async () => {
+    const loadPlantsMock = new DbLoadPlantsMock()
+    const fakePlants = mockPlants()
+    loadPlantsMock.output = fakePlants
+    const sut = new LoadPlantsController(loadPlantsMock)
+
+    const plants = await sut.handle({})
+
+    expect(plants).toEqual({
+      statusCode: 200,
+      body: fakePlants,
+    })
   })
 })
