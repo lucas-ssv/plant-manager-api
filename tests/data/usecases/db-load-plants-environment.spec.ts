be06bfc5c5caeae8dc2cdfdb4c5f9a4d3f756785
Lucas Vieira <lucas_99silva@hotmail.com>
2023-11-05 12:49:02 -0300

    feat: ensure DbLoadPlantsEnvironment returns a list of environment with plants on success

--- tests/data/usecases/db-load-plants-environment.spec.ts
+++ tests/data/usecases/db-load-plants-environment.spec.ts
@@ -1,45 +1,85 @@
 import { type LoadPlantsEnvironment } from '@/domain/usecases/load-plants-environment'
+import { faker } from '@faker-js/faker'
 
 class DbLoadPlantsEnvironment implements LoadPlantsEnvironment {
   constructor(
     private readonly loadPlantsEnvironmentRepository: LoadPlantsEnvironmentRepository
   ) {}
 
-  async perform(environment?: string): Promise<LoadPlantsEnvironment.Result> {
-    await this.loadPlantsEnvironmentRepository.loadMany()
+  async perform(environment?: string): Promise<LoadPlantsEnvironment.Result[]> {
+    return await this.loadPlantsEnvironmentRepository.loadMany()
   }
 }
 
 interface LoadPlantsEnvironmentRepository {
   loadMany: (
     environment?: string
-  ) => Promise<LoadPlantsEnvironmentRepository.Result>
+  ) => Promise<LoadPlantsEnvironmentRepository.Result[]>
 }
 
 namespace LoadPlantsEnvironmentRepository {
   export interface Result extends LoadPlantsEnvironment.Result {}
 }
 
-class LoadPlantsEnvironmentRepositoryMock
+const plantsEnvironmentModel = (): LoadPlantsEnvironment.Result[] => [
+  {
+    environment: {
+      id: faker.string.uuid(),
+      title: faker.lorem.words(),
+    },
+    plants: [
+      {
+        id: faker.string.uuid(),
+        name: faker.word.words(),
+        description: faker.lorem.words(),
+        photo: faker.image.url(),
+        waterTips: faker.lorem.words(),
+        plantWaterFrequency: {
+          id: faker.string.uuid(),
+          description: faker.word.words(),
+          gap: faker.number.int(1),
+          time: faker.number.int(1),
+          lastDateWatering: new Date(),
+        },
+      },
+    ],
+  },
+]
+
+class LoadPlantsEnvironmentRepositorySpy
   implements LoadPlantsEnvironmentRepository
 {
   callsCount = 0
+  output = plantsEnvironmentModel()
 
   async loadMany(
     environment?: string
-  ): Promise<LoadPlantsEnvironmentRepository.Result> {
+  ): Promise<LoadPlantsEnvironmentRepository.Result[]> {
     this.callsCount++
+    return this.output
   }
 }
 
 describe('DbLoadPlantsEnvironment', () => {
   it('should call LoadPlantsEnvironmentRepository', async () => {
-    const loadPlantsEnvironmentRepositoryMock =
-      new LoadPlantsEnvironmentRepositoryMock()
-    const sut = new DbLoadPlantsEnvironment(loadPlantsEnvironmentRepositoryMock)
+    const loadPlantsEnvironmentRepositorySpy =
+      new LoadPlantsEnvironmentRepositorySpy()
+    const sut = new DbLoadPlantsEnvironment(loadPlantsEnvironmentRepositorySpy)
 
     await sut.perform()
 
-    expect(loadPlantsEnvironmentRepositoryMock.callsCount).toBe(1)
+    expect(loadPlantsEnvironmentRepositorySpy.callsCount).toBe(1)
+  })
+
+  it('should return a list of environment with plants on success', async () => {
+    const loadPlantsEnvironmentRepositorySpy =
+      new LoadPlantsEnvironmentRepositorySpy()
+    const sut = new DbLoadPlantsEnvironment(loadPlantsEnvironmentRepositorySpy)
+
+    const plantsEnvironmentList = await sut.perform()
+
+    expect(plantsEnvironmentList).toEqual(
+      loadPlantsEnvironmentRepositorySpy.output
+    )
   })
 })
