46c498e335dd5c0382e4fcdf0fc2d5f0727ce7b6
Lucas Vieira <lucas_99silva@hotmail.com>
2023-11-05 12:28:21 -0300

    feat: add LoadPlantsEnvironment usecase

---
+++ src/domain/usecases/load-plants-environment.ts
@@ -0,0 +1,12 @@
+import { type Environment, type Plant } from '@/domain/entities'
+
+export interface LoadPlantsEnvironment {
+  loadMany: (environment?: string) => Promise<LoadPlantsEnvironment.Result>
+}
+
+export namespace LoadPlantsEnvironment {
+  export interface Result {
+    environment: Environment
+    plants: Omit<Plant[], 'environments'>
+  }
+}
