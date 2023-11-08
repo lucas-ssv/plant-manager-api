5e10bc0d8ccb863ed78fda79f83b230775a354d2
Lucas Vieira <lucas_99silva@hotmail.com>
2023-11-08 10:01:49 -0300

    feat: ensure LoadPlantsEnvironmentController returns 200 if LoadPlantsEnvironment returns a list of plants environment

--- tests/presentation/controllers/load-plants-environment-controller.spec.ts
+++ tests/presentation/controllers/load-plants-environment-controller.spec.ts
@@ -4,21 +4,28 @@ import {
   type HttpResponse,
   type Controller,
 } from '@/presentation/contracts'
+import { ok } from '@/presentation/helpers'
+import { plantsEnvironmentModel } from '@/tests/domain/mocks'
 import { faker } from '@faker-js/faker'
 
 class LoadPlantsEnvironmentController implements Controller {
   constructor(private readonly loadPlantsEnvironment: LoadPlantsEnvironment) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadPlantsEnvironment.perform(httpRequest.query?.q)
+    const plantsEnvironment = await this.loadPlantsEnvironment.perform(
+      httpRequest.query?.q
+    )
+    return ok(plantsEnvironment)
   }
 }
 
 class LoadPlantsEnvironmentMock implements LoadPlantsEnvironment {
   input?: string
+  output = plantsEnvironmentModel()
 
   async perform(environment?: string): Promise<LoadPlantsEnvironment.Result[]> {
     this.input = environment
+    return this.output
   }
 }
 
@@ -33,5 +40,14 @@ describe('LoadPlantsEnvironmentController', () => {
     await sut.handle(httpRequest)
 
     expect(loadPlantsEnvironmentMock.input).toBe(httpRequest.query.q)
+  })
+
+  it('should return 200 if LoadPlantsEnvironment returns a list of plants environment', async () => {
+    const loadPlantsEnvironmentMock = new LoadPlantsEnvironmentMock()
+    const sut = new LoadPlantsEnvironmentController(loadPlantsEnvironmentMock)
+
+    const httpResponse = await sut.handle({})
+
+    expect(httpResponse).toEqual(ok(loadPlantsEnvironmentMock.output))
   })
 })
