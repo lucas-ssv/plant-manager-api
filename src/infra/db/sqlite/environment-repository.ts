5e4bb810f581dce507d346037bc38d83e90b281f
Lucas Vieira <lucas_99silva@hotmail.com>
2023-11-10 15:23:01 -0300

    feat: ensure EnvironmentRepository loads all environments

--- src/infra/db/sqlite/environment-repository.ts
+++ src/infra/db/sqlite/environment-repository.ts
@@ -1,12 +1,22 @@
-import { type AddEnvironmentRepository } from '@/data/contracts'
+import {
+  type LoadEnvironmentsRepository,
+  type AddEnvironmentRepository,
+} from '@/data/contracts'
+import { type Environment } from '@/domain/entities'
 
 import { prisma } from '@/infra/db/config'
 
-export class SQLiteEnvironmentRepository implements AddEnvironmentRepository {
+export class SQLiteEnvironmentRepository
+  implements AddEnvironmentRepository, LoadEnvironmentsRepository
+{
   async add(input: AddEnvironmentRepository.Params): Promise<string> {
     const environment = await prisma.environment.create({
       data: input,
     })
     return environment.id
+  }
+
+  async loadMany(): Promise<Environment[]> {
+    return await prisma.environment.findMany()
   }
 }
