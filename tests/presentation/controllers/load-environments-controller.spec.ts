9192a8cf499bb5321c78fe4c77324df5b633ee9f
Lucas Vieira <lucas_99silva@hotmail.com>
2023-11-10 15:38:40 -0300

    feat: ensure LoadEnvironmentsController returns 200 if LoadEnvironments returns a list of environments

--- tests/presentation/controllers/load-environments-controller.spec.ts
+++ tests/presentation/controllers/load-environments-controller.spec.ts
@@ -5,20 +5,36 @@ import {
   type HttpRequest,
   type HttpResponse,
 } from '@/presentation/contracts'
+import { ok } from '@/presentation/helpers'
+import { faker } from '@faker-js/faker'
 
 class LoadEnvironmentsController implements Controller {
   constructor(private readonly loadEnvironments: LoadEnvironments) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadEnvironments.perform()
+    const environments = await this.loadEnvironments.perform()
+    return ok(environments)
   }
 }
 
+const mockEnvironments = (): Environment[] => [
+  {
+    id: faker.string.uuid(),
+    title: faker.word.words(),
+  },
+  {
+    id: faker.string.uuid(),
+    title: faker.word.words(),
+  },
+]
+
 class LoadEnvironmentsMock implements LoadEnvironments {
   callsCount = 0
+  output = mockEnvironments()
 
   async perform(): Promise<Environment[]> {
     this.callsCount++
+    return this.output
   }
 }
 
@@ -30,5 +46,14 @@ describe('LoadEnvironmentsController', () => {
     await sut.handle({})
 
     expect(loadEnvironmentsMock.callsCount).toBe(1)
+  })
+
+  it('should return 200 if LoadEnvironments returns a list of environments', async () => {
+    const loadEnvironmentsMock = new LoadEnvironmentsMock()
+    const sut = new LoadEnvironmentsController(loadEnvironmentsMock)
+
+    const httpResponse = await sut.handle({})
+
+    expect(httpResponse).toEqual(ok(loadEnvironmentsMock.output))
   })
 })
